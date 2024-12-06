f95db75db91d27191c68c7db5eb10991fb2c0539
Klee Collage <klee.collage@gmail.com>
2024-12-06 14:05:50 -0600

    chore(module): Set enviroment, add .env templates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import configurationMongo from 'src/configuration/configuration-mongo';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configurationMongo],
+      envFilePath: `./env/${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb+srv://${configService.get('mongo.user')}:${configService.get('mongo.password')}@${configService.get('mongo.host')}/${configService.get('mongo.database')}?retryWrites=true&w=majority&appName=AuthNestJS`,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
