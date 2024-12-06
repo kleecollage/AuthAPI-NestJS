4a0e63785ce7bffc80c57c0339fb9cc034be9dd5
Klee Collage <klee.collage@gmail.com>
2024-12-06 16:25:27 -0600

    feat(service): Add jwtStrategy for authentication user

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import bcrypt from 'bcryptjs';
+import { AuthCredentialsDto } from 'src/modules/auth/dto/auth-credentials-dto';
+import { UsersService } from 'src/modules/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(private userService: UsersService) {}
+
+  async validateUser(userCredentials: AuthCredentialsDto) {
+    const user = await this.userService.getUserByEmail(userCredentials.email);
+
+    if (user) {
+      const passwordOk = bcrypt.compare(
+        userCredentials.password,
+        user.password,
+      );
+
+      if (passwordOk) return user;
+    }
+    return null;
+  }
 }
--- src/modules/auth/strategy/jwt-strategy/jwt-strategy.service.ts
+++ src/modules/auth/strategy/jwt-strategy/jwt-strategy.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from 'src/modules/auth/dto/jwt-payload';
+import { UsersService } from 'src/modules/users/users.service';
 
 @Injectable()
-export class JwtStrategyService {}
+export class JwtStrategyService extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private userService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('auth.secretKey'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.userService.getUserByEmail(payload.email);
+    if (!user) throw new UnauthorizedException();
+
+    user.password = undefined;
+
+    return user;
+  }
+}
