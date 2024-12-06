f2330fd7bf897cdf6ac3886b877241912740849a
Klee Collage <klee.collage@gmail.com>
2024-12-06 17:38:06 -0600

    docs(controller): Add Swagger decorators for documentation on UsersController

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthService } from 'src/modules/auth/auth.service';
 import { AuthCredentialsDto } from 'src/modules/auth/dto/auth-credentials-dto';
 
@@ -9,7 +16,44 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/login')
+  @ApiOperation({
+    description: 'Log in the app',
+  })
+  @ApiBody({
+    description: 'Logs in the app using the credentials',
+    type: AuthCredentialsDto,
+    examples: {
+      example: {
+        value: {
+          email: 'test1@mail.com',
+          password: '123456',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Login oks',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Invalid credentials',
+  })
   login(@Body() authCredentials: AuthCredentialsDto) {
     return this.authService.login(authCredentials);
+  }
+
+  @Get('data-user')
+  @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({
+    description: 'Return info of the logued user',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiBearerAuth('jwt')
+  dataUser(@Req() request) {
+    return request.user;
   }
 }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,13 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AuthCredentialsDto } from 'src/modules/auth/dto/auth-credentials-dto';
 import { UserDto } from 'src/modules/users/dto/user-dto';
 import { UsersService } from 'src/modules/users/users.service';
 
@@ -11,12 +18,52 @@ export class UsersController {
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({
+    description: 'Create new user',
+  })
+  @ApiBody({
+    description: 'Creates a user using UserDto',
+    type: AuthCredentialsDto,
+    examples: {
+      example: {
+        value: {
+          email: 'test1@mail.com',
+          password: '123456',
+        },
+      },
+    },
+  })
+  @ApiBearerAuth('jwt')
+  @ApiResponse({
+    status: 201,
+    description: 'User created successfully',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'User already exists',
+  })
   createUser(@Body() user: UserDto) {
     return this.userService.createUser(user);
   }
 
   @Get()
   @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({
+    description: 'Return all users',
+  })
+  @ApiBearerAuth('jwt')
+  @ApiResponse({
+    status: 201,
+    description: 'Users returned successfully',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
   getUsers() {
     return this.userService.getUsers();
   }
